4940f3ca4cb5a8ccdc3af325a3f2c075e501e877
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2021-05-16 19:56:10 +0530

    first ever js code written

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    // get values
+    var principal = validatePrincipal();
+    if (isNaN(principal) || principal <= 0) {
+        console.error("not a valid number.");
+        return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    // compute stuff
+    var interest = (principal * rate * years) / 100; // the interest
+    var year = new Date().getFullYear() + parseInt(years); // the future year
+
+    // output stuff
+    var result = document.getElementById("result");
+    var outputString = `If you deposit <mark>${principal}</mark>,<br>
+    at an interest rate of <mark>${rate}%</mark>.<br>
+    You will receive an amount of <mark>${interest}</mark>,<br>
+    in the year <mark>${year}</mark>`;
+    result.innerHTML = outputString;
 }
-        
+
+function updateRate() {
+    var rateVal = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rateVal + "%";
+}
+
+function validatePrincipal() {
+    var value = document.getElementById("principal");
+    var converted = parseInt(value.value);
+    if (isNaN(converted)) {
+        alert("Please enter a valid number using digits");
+        value.focus();
+    } else if (converted <= 0) {
+        alert("Enter a positive number");
+        value.focus();
+    }
+    value.focus();
+    document.getElementById("principal").focus();
+    return converted;
+}
